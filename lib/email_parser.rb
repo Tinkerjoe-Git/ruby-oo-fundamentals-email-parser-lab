0ee9ac5c142d6c3ecfbddb332af1c0316fdf1650
Tinkergit <nicholas.stagno@gmail.com>
2021-02-08 18:13:20 -0500

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,29 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+class EmailAddressParser
+    attr_accessor :email_list
+
+    def initialize(email_list)
+        @email_list=email_list    
+    end
+
+    def parse
+        @email_list.split(/,?*\s/).uniq #using regex, searching for several delimiter scenarios
+
+    end
+    
+    ######        if email_list.include?(", ")  The Hard way lol.
+   # @list=email_list.split(", ")
+#elsif email_list.include?(" ")
+ #@list=email_list.split(" ")
+#elsif email_list.include?(", ") && email_list.include?(" ")
+  #  emptyindex=email_list.index(" ")
+   # email_list[emptyindex]=", "
+    #@list=email_list.split(", ")
+  
+end
+
+
+
